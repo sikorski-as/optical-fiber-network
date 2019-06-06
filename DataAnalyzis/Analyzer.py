7563af9d13477bf6b61d8b6163926d796b0f6491
kacperwnuk <kacperwnuk97@o2.pl>
2019-06-06 21:02:17 +0200

    Upload file with edges

--- DataAnalyzis/Analyzer.py
+++ DataAnalyzis/Analyzer.py
@@ -1,20 +1,25 @@
+from pprint import pprint
+
 import numpy as np
 
 
 class DataInfo:
     """
         transponders -> key - int, value -> (x, y) x - bitrate, y - slices used
-        vertices -> amount of cities
+        number_of_vertices -> amount of cities
     """
 
-    def __init__(self, file):
+    def __init__(self, result_file, paths_file):
         self.demand = 450
         self.transponders = {1: (40, 2), 2: (100, 3), 3: (200, 4), 4: (400, 6)}
         self.number_of_slices = 768
         self.number_of_paths = 3
-        self.vertices = 12
-        self.file = file
-        self.matrix = ([[[np.zeros((self.number_of_slices, self.vertices), dtype=int) for k in range(0, self.number_of_paths)] for j in range(0, self.vertices)] for
+        self.number_of_vertices = 12
+        self.number_of_edges = 18
+        self.file = result_file
+        self.paths_file = paths_file
+        self.paths_dict = {(i, j): [list() for _ in range(0, self.number_of_paths)] for i in range(0, self.number_of_vertices) for j in range(0, self.number_of_vertices) if i != j}
+        self.matrix = ([[[np.zeros((self.number_of_slices, self.number_of_vertices), dtype=int) for k in range(0, self.number_of_paths)] for j in range(0, self.number_of_vertices)] for
         i in range(0, len(self.transponders.keys()))])
 
     def skip_line(self):
@@ -22,7 +27,7 @@ class DataInfo:
 
     def upload_data(self):
         for i in range(0, len(self.transponders.keys())):
-            for j in range(0, self.vertices):
+            for j in range(0, self.number_of_vertices):
                 for k in range(0, self.number_of_paths):
                     self.skip_line()
                     self.skip_line()
@@ -33,13 +38,31 @@ class DataInfo:
 
     def statistics(self):
         for i in range(0, len(self.transponders.keys())):
-            for j in range(0, self.vertices):
+            for j in range(0, self.number_of_vertices):
                 for k in range(0, self.number_of_paths):
                     print(f"{i}:{j}:{k} -> {self.matrix[i][j][k].sum()}")
 
+    def upload_paths(self):
+        self.paths_file.readline()
+        for _ in range(0, self.number_of_edges):
+            for _ in range(0, self.number_of_paths):
+                for _ in range(0, self.number_of_vertices):
+                    vertice, path, edge, *row = self.paths_file.readline().split()
+                    vertice_number, path_number, edge_number = int(vertice), int(path), int(edge)
+                    for i, result in enumerate(row, start=0):
+                        if result == '1':
+                            key = (vertice_number - 1, i)
+                            self.paths_dict[key][path_number - 1].append(edge_number)
+                self.paths_file.readline()
+            self.paths_file.readline()
+        pprint(self.paths_dict)
+
 
 if __name__ == '__main__':
+
     with open("wyn_450.out") as file:
-        data_info = DataInfo(file)
-        data_info.upload_data()
-        data_info.statistics()
+        with open("450.dat") as path_file:
+            data_info = DataInfo(result_file=file, paths_file=path_file)
+            data_info.upload_data()
+            data_info.statistics()
+            data_info.upload_paths()
